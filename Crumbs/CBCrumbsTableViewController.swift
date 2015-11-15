56ad4fd51acff5380de8ad417768e7eb3b715c97
Daniel Rakhamimov <danielrakh@gmail.com>
2015-11-15 16:05:24 -0500

    Delete regular UIViewController for UITableViewController

---
+++ Crumbs/CBCrumbsTableViewController.swift
@@ -0,0 +1,38 @@
+//
+//  CBCrumbsTableViewController.swift
+//  Crumbs
+//
+//  Created by Daniel on 11/15/15.
+//  Copyright © 2015 Daniel Rakhamimov. All rights reserved.
+//
+
+import UIKit
+
+class CBCrumbsTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 10
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("CrumbCell") as! CBCrumbCell
+        return cell
+        
+    }
+
+
+
+}
